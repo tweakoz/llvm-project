38be1529e101509c2e3cff50e188a5adb894e965
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2026-05-08 12:20:45 -0400

    convert to exec-charset inside getPredefinedStringLiteralFromCache, test __builtin_FILE()

--- clang/lib/Lex/TextEncodingConfig.cpp
+++ clang/lib/Lex/TextEncodingConfig.cpp
@@ -37,10 +37,11 @@ TextEncodingConfig::setConvertersFromOptions(TextEncodingConfig &TEC,
     return std::error_code();
   ErrorOr<TextEncodingConverter> ErrorOrConverter =
       llvm::TextEncodingConverter::create(UTF8, TEC.ExecEncoding);
-  if (ErrorOrConverter)
+  if (ErrorOrConverter) {
     TEC.ToExecEncodingConverter =
         new TextEncodingConverter(std::move(*ErrorOrConverter));
-  else
+    TInfo.ExecStrConverter = TEC.ToExecEncodingConverter;
+  } else
     return ErrorOrConverter.getError();
 
   ErrorOrConverter = llvm::TextEncodingConverter::create(
